274de99b62075729a34ab2da3500423aaf05ab1f
Sargis Kocharyan <sargiskocharyan@yahoo.com>
2019-10-08 05:51:00 +0400

    Removed redundant codes

--- ImgurViewer/ImgurViewer/Constants.swift
+++ ImgurViewer/ImgurViewer/Constants.swift
@@ -10,9 +10,6 @@ import Foundation
 
 let Client_ID = "9876352d638f514"
 let Client_secret = "623abe385c13cb3f5ce57bbc9283c3c3dc84c9a1"
-let Access_token = "c983c687b8ca41aa3749e90afb46ab8c07bb77df"
-let Refresh_token = "a3b8d762cf7340e57c89de6ae180675fdd65dc8c"
-let UserName = "sargisk"
-let Account_id = "115076403"
+
 
 
